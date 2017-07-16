0baf0b2091e0b51eb32218df31d65e872c96ad20
AllStarsAT <j.luckenberger@gmail.com>
2017-07-16 22:19:18 +0200

    Cards and Heroes basics

--- scripts/Card.cs
+++ scripts/Card.cs
@@ -6,15 +6,56 @@ using UnityEngine;
 public class Card : MonoBehaviour
 {
 	private String name;
-	
+	private int manacosts;
+	private int attack;
+	private int life;
+
 	Card(String newName)
 	{
 		setName(newName);
+	}
+	
+	Card(String newName, int newManacosts, int newAttack, int newLife)
+	{
+		setName(newName);
+		setManacosts(newManacosts);
+		setAttack(newAttack);
+		setLife(newLife);
 	}
 
 	void setName(String newName)
 	{
 		name = newName;
+	}
+
+	void setManacosts(int newManacosts)
+	{
+		manacosts = newManacosts;
+	}
+
+	void setAttack(int newAttack)
+	{
+		attack = newAttack;
+	}
+
+	void setLife(int newLife)
+	{
+		life = newLife;
+	}
+
+	int getManacosts()
+	{
+		return manacosts;
+	}
+
+	int getAttack()
+	{
+		return attack;
+	}
+
+	int getLife()
+	{
+		return life;
 	}
 	
 	// Use this for initialization
