fa0d4bef00526118a003c9c7c451461bfac06f50
DavidTS <10davdav@gmail.com>
2017-07-16 21:05:10 +0200

    changed a thing

--- scripts/control.cs
+++ scripts/control.cs
@@ -6,7 +6,7 @@ public class control : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		print("very sexy");
+		print("very sexy asgasdgas");
 	}
 	
 	// Update is called once per frame
