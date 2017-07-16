987a539768422f043b27f126051d4bb8d7294e40
DavidTS <10davdav@gmail.com>
2017-07-16 21:23:40 +0200

    a nother change

--- scripts/control.cs
+++ scripts/control.cs
@@ -7,10 +7,11 @@ public class control : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		print("Not magic is way better than magic!");
+		print("a thing");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		print("very sexy LUTTI.-.");
+		print("");
 	}
 }
