ff7bc22a220bb667b5029f6ef6ce5f5670bd59d4
AllStarsAT <j.luckenberger@gmail.com>
2017-07-16 21:23:08 +0200

    cool guys

--- scripts/control.cs
+++ scripts/control.cs
@@ -6,7 +6,7 @@ public class control : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		print("Not magic is way better than magic!");
+		print("Not magic is way better than magic cuz its made by cooler guys haha!");
 	}
 	
 	// Update is called once per frame
