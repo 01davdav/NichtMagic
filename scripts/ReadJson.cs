e67fa80ac4b5ce2cc798ea0efffc8f8e0566f2c7
DavidTS <10davdav@gmail.com>
2017-07-18 00:36:11 +0200

    reading databases
    and other stuff

---
+++ scripts/ReadJson.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using LitJson;
+
+public class ReadJson
+{
+	private static string _jsonString = File.ReadAllText(Application.dataPath + "/other/cards.json");
+	private static JsonData _cardData = JsonMapper.ToObject(_jsonString);
+	
+
+	public static Card GetCard(int id)
+	{
+		return new Card((string)_cardData["Cards"][0]["name"],(int)_cardData["Cards"][0]["cost"],(int)_cardData["Cards"][0]["attack"],(int)_cardData["Cards"][0]["life"],(int)_cardData["Cards"][0]["shield"],(string)_cardData["Cards"][0]["path"]);
+	}
+}
--- scripts/control.cs
+++ scripts/control.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 using Random = UnityEngine.Random;
